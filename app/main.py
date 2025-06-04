b211ebcd5666892073121173d3f66a1e1d621459
Андрей _ <ashulyanskiy0000@gmail.com>
2025-06-04 18:05:28 +0300

    initialize

---
+++ app/main.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Result:", add(4, 7))  # Очікувано: 11
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+from app.main import add
+
+def test_add():
+    assert add(4, 7) == 11
